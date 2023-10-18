fee388de64dc34b954791a4c283619ca56515fa7
doppedheart <anuragagarwal530@gmail.com>
2023-10-18 18:09:34 +0530

    changes in the deployment file

--- script/DeployToken.s.sol
+++ script/DeployToken.s.sol
@@ -5,7 +5,7 @@ import {Script} from "forge-std/Script.sol";
 import {OurToken} from "../src/OurToken.sol";
 import {console} from "forge-std/console.sol";
 
-contract DeployOurToken is Script {
+contract DeployToken is Script {
     uint256 public constant INITIAL_SUPPLY = 1_000_000 ether; // 1 million tokens with 18 decimal places
     uint256 public DEFAULT_ANVIL_PRIVATE_KEY =
         0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
@@ -15,7 +15,7 @@ contract DeployOurToken is Script {
         if (block.chainid == 31337) {
             deployerKey = DEFAULT_ANVIL_PRIVATE_KEY;
         } else {
-            deployerKey = vm.envUint("PRIVATE_KEY");
+            deployerKey = vm.envUint("SEPOLIA_PRIVATE_KEY");
         }
         vm.startBroadcast(deployerKey);
         OurToken ourToken = new OurToken(INITIAL_SUPPLY);
